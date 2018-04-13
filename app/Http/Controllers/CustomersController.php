973242cb9162accd3bdf99c7edb40fc2e5afab46
Gbolahan Olawuyi <gbolahanolawuyi4u@gmail.com>
2018-04-13 06:00:45 +0100

    Added index, show, store methods to customerscontroller, Added routes for customers to api

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,9 +2,50 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
 use Illuminate\Http\Request;
 
 class CustomersController extends Controller
 {
-    //
+    public function index()
+    {
+        $customers = Customer::all();
+        // $data = [
+        //     'id' => $customers->id,
+        //     'first_name' => $customers->first_name,
+        //     'last_name' => $customers->last_name,
+        //     'phone' => $customers->phone,
+        //     'address' => $customers->address,
+        //     'city' => $customers->city,
+        //     'state' => $customers->state
+        // ];
+        return response()->json($customers);
+    }
+    public function show($id)
+    {
+        $customer = Customer::find($id);
+        return response()->json($customer);
+    }
+
+    public function store()
+    {
+        $this->validate(request(), [
+            'first_name' =>'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'state' => 'required'
+        ]);
+        $customer = Customer::create([
+            'first_name' => request()->first_name,
+            'last_name' => request()->last_name,
+            'phone' => request()->phone,
+            'address' => request()->address,
+            'city' => request()->city,
+            'state' => request()->state
+        ]);
+
+        return response("Success: Customer Added", 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,9 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/customers', 'CustomersController@index');
+
+Route::get('/customer/{id}', 'CustomersController@show');
+
+Route::post('/customer/add', 'CustomersController@store');
